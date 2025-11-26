0222d07408cae701f52cb051f923c70c57806131
Deva Sai Sunder Tangella <79849464+devt-code@users.noreply.github.com>
2025-11-26 01:24:36 -0500

    Added async to enrollments routes

--- Kambaz/Enrollments/routes.js
+++ Kambaz/Enrollments/routes.js
@@ -3,21 +3,21 @@ import EnrollmentsDao from "./dao.js";
 export default function EnrollmentsRoutes(app, db) {
   const dao = EnrollmentsDao(db);
 
-  const enrollUserInCourse = (req, res) => {
+  const enrollUserInCourse = async (req, res) => {
     const { userId, courseId } = req.params;
-    const enrollment = dao.enrollUserInCourse(userId, courseId);
+    const enrollment = await dao.enrollUserInCourse(userId, courseId);
     res.json(enrollment);
   };
 
-  const unenrollUserFromCourse = (req, res) => {
+  const unenrollUserFromCourse = async (req, res) => {
     const { userId, courseId } = req.params;
-    const result = dao.unenrollUserFromCourse(userId, courseId);
+    const result = await dao.unenrollUserFromCourse(userId, courseId);
     res.json(result);
   };
 
-  const findEnrollmentsForUser = (req, res) => {
+  const findEnrollmentsForUser = async (req, res) => {
     const { userId } = req.params;
-    const enrollments = dao.findEnrollmentsForUser(userId);
+    const enrollments = await dao.findEnrollmentsForUser(userId);
     res.json(enrollments);
   };
 
