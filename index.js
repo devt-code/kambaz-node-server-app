bce5a7780f428627160fe39d599f84854a4c58ec
Deva Sai Sunder Tangella <79849464+devt-code@users.noreply.github.com>
2025-12-02 19:50:35 -0500

    Added quizzess code

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import AssignmentsRoutes from "./Kambaz/Assignments/routes.js";
 import EnrollmentsRoutes from "./Kambaz/Enrollments/routes.js";
 import db from "./Kambaz/Database/index.js";
 import UserRoutes from "./Kambaz/Users/routes.js";
+import QuizRoutes from "./Kambaz/Quizzes/route.js";
 import cors from "cors";
 import Hello from "./hello.js";
 
@@ -47,6 +48,7 @@ CourseRoutes(app, db);
 ModulesRoutes(app, db);
 AssignmentsRoutes(app, db);
 EnrollmentsRoutes(app, db);
+QuizRoutes(app);
 
 Lab5(app);
 Hello(app);
